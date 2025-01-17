b4185dda7084d6e88fd19531a3abd4a2277911a1
onixtsm <alexander@sautkin.com>
2025-01-17 18:12:31 +0100

    Working nvim

--- lua/onix/remap.lua
+++ lua/onix/remap.lua
@@ -8,6 +8,7 @@ end)
 
 vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
 vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
+vim.keymap.set("n", "E", "ge", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<c-d>", "<c-d>zz")
 vim.keymap.set("n", "<c-u>", "<c-u>zz")
